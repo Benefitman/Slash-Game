5517540e2f720eb72efd128e404b63b2a003ae9e
Ferid <ferid.malkoc@gmail.com>
2024-06-10 21:06:33 +0200

    Added treasure that drops from destroyed pots

--- Source/Slash/Public/Breakable/BreakableActor.h
+++ Source/Slash/Public/Breakable/BreakableActor.h
@@ -21,9 +21,17 @@ public:
 	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
 
 protected:
+	
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UGeometryCollectionComponent* GeometryCollection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCapsuleComponent* Capsule;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Breakable Properties")
+	TSubclassOf<class ATreasure> TreasureClass;
 };
