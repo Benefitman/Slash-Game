6aec8a1a8955c515769367854768cdbe761af4ad
Ferid <ferid.malkoc@gmail.com>
2024-06-10 14:34:19 +0200

    implemented breakable obj so that destructable envoirement is possible

--- Source/Slash/Private/Item/Weapon/Weapon.cpp
+++ Source/Slash/Private/Item/Weapon/Weapon.cpp
@@ -89,5 +89,7 @@ void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 			HitInterface->GetHit(BoxHit.ImpactPoint);
 		}
 		IgnoreActors.AddUnique(BoxHit.GetActor());
+
+		CreateFields(BoxHit.ImpactPoint);
 	}
 }
