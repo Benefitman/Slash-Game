27dc4ef898837df456746b5c6fac794009823203
Ferid <ferid.malkoc@gmail.com>
2024-06-12 10:51:41 +0200

    Improved AI: working chasing, loosing interest and attacking state added

--- Source/Slash/Private/Enemy/Enemy.cpp
+++ Source/Slash/Private/Enemy/Enemy.cpp
@@ -146,12 +146,15 @@ void AEnemy::PawnSeen(APawn* SeenPawn)
 	if (EnemyState == EEnemyState::EES_Chasing) return;
 	if (SeenPawn->ActorHasTag(FName("SlashCharacter")))
 	{
-		EnemyState = EEnemyState::EES_Chasing;
 		GetWorldTimerManager().ClearTimer(PatrolTimer);
 		GetCharacterMovement()->MaxWalkSpeed = 300.f;
 		CombatTarget = SeenPawn;
-		MoveToTarget(CombatTarget);
-		UE_LOG(LogTemp, Warning, TEXT("Seen Pawn, Now Chasing"));
+		if (EnemyState != EEnemyState::EES_Attacking)
+		{
+			EnemyState = EEnemyState::EES_Chasing;
+			MoveToTarget(CombatTarget);
+			UE_LOG(LogTemp, Warning, TEXT("Pawn Seen, Chase Player!"));
+		}
 	}
 }
 
@@ -169,11 +172,31 @@ void AEnemy::CheckCombatTarget()
 {
 	if (!InTargetRange(CombatTarget, CombatRadius))
 	{
+		//Outside combat radius lets him loose interest.
 		CombatTarget = nullptr;
 		if (HealthBarWidget)
 		{
 			HealthBarWidget->SetVisibility(false);
 		}
+		EnemyState = EEnemyState::EES_Patrolling;
+		GetCharacterMovement()->MaxWalkSpeed = 125.f;
+		MoveToTarget(PatrolTarget);
+		UE_LOG(LogTemp, Warning, TEXT("Lose Interest!"));
+	}
+	else if (!InTargetRange(CombatTarget, AttackRadius) && EnemyState != EEnemyState::EES_Chasing)
+	{
+		//Outside of the attack range, chase the main character
+		EnemyState = EEnemyState::EES_Chasing;
+		GetCharacterMovement()->MaxWalkSpeed = 300.f;
+		MoveToTarget(CombatTarget);
+		UE_LOG(LogTemp, Warning, TEXT("Chase Player!"));
+	}
+	else if (InTargetRange(CombatTarget, AttackRadius) && EnemyState != EEnemyState::EES_Attacking)
+	{
+		//Inside attack range, attack character
+		EnemyState = EEnemyState::EES_Attacking;
+		//TODO: Attack montage
+		UE_LOG(LogTemp, Warning, TEXT("Attack Player!"));
 	}
 }
 
--- Source/Slash/Public/Enemy/Enemy.h
+++ Source/Slash/Public/Enemy/Enemy.h
@@ -69,6 +69,9 @@ private:
 	UPROPERTY()
 	double CombatRadius = 1000.f;
 
+	UPROPERTY()
+	double AttackRadius = 150.f;
+
 	/**
 	 * Navigation
 	 * */
