222f980c3ec3cad6b020acc9c830bdeb392a5adb
Ferid <ferid.malkoc@gmail.com>
2024-06-13 10:33:41 +0200

    expected Enemy.h to be first

--- Source/Slash/Private/Enemy/Enemy.cpp
+++ Source/Slash/Private/Enemy/Enemy.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Enemy/Enemy.h"
 #include "AIController.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Components/AttributeComponent.h"
 #include "Components/ActorComponent.h"
-#include "Enemy/Enemy.h"
 #include "Perception/PawnSensingComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "HUD/HealthBarComponent.h"
